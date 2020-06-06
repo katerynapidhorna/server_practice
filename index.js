aa74e47298c72527c5473dfe495f124da5f81423
katerynapidhorna <katerynapidhorna@gmail.com>
2020-06-06 06:51:43 +0200

    add greeting responses

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const app = express();
 
 const port = 4000;
 
-function greet(request, response) {
+//greeting functions
+function greetInit(request, response) {
   response.send('Welcome')
+  console.log('running!')
+}
+
+function personalGreeting(request, response) {
+  const userName = request.params.name;
+  const message = `Hello ${userName}, how are you today?`
+  response.send(message);
 }
 
 
+                          //endpoints***************************//
 
+// welcome user
+app.get('/user/:name',personalGreeting)
 
-
-
-//endpoints
-
-app.get('/', greet)
+//initial endpoint
+app.get('/', greetInit)
 
 
 
