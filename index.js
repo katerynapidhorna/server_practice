85d946dd340e5ec27c84ff4671db7cac3e995c4a
katerynapidhorna <katerynapidhorna@gmail.com>
2020-06-06 07:04:19 +0200

    add API server

--- index.js
+++ index.js
@@ -5,27 +5,41 @@ const app = express();
 const port = 4000;
 
 //greeting functions
-function greetInit(request, response) {
-  response.send('Welcome')
-  console.log('running!')
-}
+// function greetInit(request, response) {
+//   response.send('Welcome')
+//   console.log('running!')
+// }
 
-function personalGreeting(request, response) {
-  const userName = request.params.name;
-  const message = `Hello ${userName}, how are you today?`
-  response.send(message);
-}
+// function personalGreeting(request, response) {
+//   const userName = request.params.name;
+//   const message = `Hello ${userName}, how are you today?`
+//   response.send(message);
+// }
 
+// object with data
+const userData = [
+    {
+      name: 'Bob',
+      age: 45,
+      gender: 'm'
+    },
+    {
+      name:'Lisa',
+      age: 38,
+      gender:'f'
+    }
+]
+
+
+function userInfo(request,response) {
+  response.send(userData);
+  console.log('data on the client')
+}
 
                           //endpoints***************************//
 
-// welcome user
-app.get('/user/:name',personalGreeting)
-
-//initial endpoint
-app.get('/', greetInit)
-
-
+//user information
+app.get('/user-info',userInfo)
 
 
 
